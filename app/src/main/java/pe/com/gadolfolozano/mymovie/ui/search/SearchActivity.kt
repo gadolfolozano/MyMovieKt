fba6c6eedabf945a4af905e8ba8643a5378f385a
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2019-02-06 23:46:53 -0500

    add recyclerview for movies in SearchActivity

--- app/src/main/java/pe/com/gadolfolozano/mymovie/ui/search/SearchActivity.kt
+++ app/src/main/java/pe/com/gadolfolozano/mymovie/ui/search/SearchActivity.kt
@@ -3,9 +3,11 @@ package pe.com.gadolfolozano.mymovie.ui.search
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import pe.com.gadolfolozano.mymovie.BR
 import pe.com.gadolfolozano.mymovie.R
 import pe.com.gadolfolozano.mymovie.databinding.ActivitySearchBinding
+import pe.com.gadolfolozano.mymovie.model.MovieModel
 import pe.com.gadolfolozano.mymovie.ui.base.BaseActivity
 import pe.com.gadolfolozano.mymovie.ui.main.MainActivity
 import javax.inject.Inject
@@ -30,6 +32,26 @@ class SearchActivity : BaseActivity<ActivitySearchBinding, SearchViewModel>(), S
         mSearchViewModel.navigator = this
 
         mBinding = getViewDataBinding()
+
+        mBinding?.recyclerView?.layoutManager = LinearLayoutManager(this)
+
+        var movies = ArrayList<MovieModel>()
+        movies.add(MovieModel("movie 1"))
+        movies.add(MovieModel("movie 2"))
+        movies.add(MovieModel("movie 3"))
+        movies.add(MovieModel("movie 4"))
+
+        mBinding?.recyclerView?.adapter = SearchMovieAdapter(movies)
+        mBinding?.button?.setOnClickListener {
+            onButtonAddClicked()
+        }
+    }
+
+    fun onButtonAddClicked() {
+        if (!mBinding?.editText?.text.isNullOrEmpty()) {
+            (mBinding?.recyclerView?.adapter as SearchMovieAdapter)
+                .addMoview(MovieModel(mBinding?.editText?.text.toString()))
+        }
     }
 
     override fun openMain() {
